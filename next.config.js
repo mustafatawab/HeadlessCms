bae6838d3071ecd3932ea81968267d6e6c4509dc
Mustafa Tawab <115402517+mustafatawab@users.noreply.github.com>
2023-04-07 04:02:28 +0500

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env :{
+    CONTENTFUL_SPACE_ID : "viycukvlh0rc",
+    CONTENTFUL_ACCESS_KEY : '_4klXEsxP4aFBaKXZ4PvRsjVdckdiojre_BB1vUtnNw'
+  }, 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
